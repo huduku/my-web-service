8467565c2a88893f395625a19b11c6806a9d02b7
hudk <huduku.io@hotmail.com>
2025-01-30 23:34:19 +0800

    db res

--- src/service/student_services.rs
+++ src/service/student_services.rs
@@ -21,23 +21,47 @@ pub async fn get_student(rb: &RBatis, id: i64) -> Result<Student, String> {
     }
 }
 
-pub async fn create_student(rb: &RBatis, student: Student) -> Result<ExecResult, Error> {
-    Student::insert(rb, &student).await
+pub async fn create_student(rb: &RBatis, student: Student) -> Result<(), String> {
+    let DbRes(res) = Student::insert(rb, &student).await.into();
+    let res = res?;
+    if res.rows_affected == 0 {
+        return Err("操作失败!".to_string());
+    }
+    return Ok(());
 }
 
-pub async fn update_student(rb: &RBatis, student: Student) -> Result<ExecResult, Error> {
-    Student::update_by_column(rb, &student, "id").await
+pub async fn update_student(rb: &RBatis, student: Student) -> Result<(), String> {
+    let DbRes(res) = Student::update_by_column(rb, &student, "id").await.into();
+    let res = res?;
+    if res.rows_affected == 0 {
+        return Err("操作失败!".to_string());
+    }
+    return Ok(());
 }
 
-pub async fn delete_student(rb: &RBatis, id: i64) -> Result<ExecResult, Error> {
-    Student::delete_by_column(rb, "id", id).await
+pub async fn delete_student(rb: &RBatis, id: i64) -> Result<(), String> {
+    let DbRes(res) =Student::delete_by_column(rb, "id", id).await.into();
+    let res = res?;
+    if res.rows_affected == 0 {
+        return Err("操作失败!".to_string());
+    }
+    return Ok(());
 }
 
-pub async fn list_students(
-    rb: &RBatis,
-    stu_page_req: PageReq<Student>,
-) -> rbatis::Result<Page<Student>> {
+// pub async fn list_students(
+//     rb: &RBatis,
+//     stu_page_req: PageReq<Student>,
+// ) -> rbatis::Result<Page<Student>> {
+//     let page = PageRequest::from_ref(&stu_page_req);
+//     let stu = &stu_page_req.req;
+//     Student::select_page(rb, &page, &stu).await
+// }
+
+
+pub async fn list_students(rb: &RBatis, stu_page_req: PageReq<Student>,) 
+    -> Result<Page<Student>, String> {
     let page = PageRequest::from_ref(&stu_page_req);
     let stu = &stu_page_req.req;
-    Student::select_page(rb, &page, &stu).await
+    let DbRes(res) = Student::select_page(rb, &page, &stu).await.into();
+    res
 }
