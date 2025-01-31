1c1a78900b89ec2c09a7f016ac8b21f2297a9889
hudk <huduku.io@hotmail.com>
2025-01-31 08:00:00 +0800

    refactor command

---
+++ src/domain/commands/student_id_oper.rs
@@ -0,0 +1,36 @@
+use crate::domain::{
+    models::student::Student, 
+    primitives::dp::{DomainPrimitive, Id, IdOper}
+};
+
+
+impl DomainPrimitive<IdOper> for Student {
+    fn new(value: &Self) -> Result<IdOper, String> {
+        IdOper::try_from(value.clone())
+    }
+}
+
+
+impl TryFrom<Student> for IdOper {
+    type Error = String;
+    fn try_from(value: Student) -> Result<Self, Self::Error> {
+        let id = Id::new(value.id)?;
+        Ok(IdOper {
+            id 
+        })
+    }
+}
+
+impl From<IdOper> for Student {
+
+    fn from(value: IdOper) -> Self {
+        Self {
+            id: Some(value.id.0),
+            stu_no: None,
+            name: None,
+            age: None,
+            class_id: None,
+            address: None
+        }
+    }
+}
